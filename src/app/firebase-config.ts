439776bc29efc8ac1990c1f603581eadf36626d8
Lautaro Nahuel Fernandez <36265003+Lautaro01@users.noreply.github.com>
2021-04-23 23:11:27 -0300

    Delete firebase-config.ts

--- src/app/firebase-config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyDGzjDGaqG1wOsyBXXgDQcqV7VMFRVl73w",
-    authDomain: "saladejuegosv2.firebaseapp.com",
-    projectId: "saladejuegosv2",
-    storageBucket: "saladejuegosv2.appspot.com",
-    messagingSenderId: "10178501775",
-    appId: "1:10178501775:web:9b5604bf05a323402e24fa",
-    measurementId: "G-5TET92SQ5K"
-};
